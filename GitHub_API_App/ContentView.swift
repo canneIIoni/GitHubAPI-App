d01fe488937354ef394a934298037b3aacc10d67
canneIIoni <paesberaldiluca@gmail.com>
2023-08-15 15:50:31 -0300

    Initial Commit

---
+++ GitHub_API_App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHub_API_App
+//
+//  Created by Luca on 15/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub_API_App/GitHub_API_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_API_AppApp.swift
+//  GitHub_API_App
+//
+//  Created by Luca on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_API_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
