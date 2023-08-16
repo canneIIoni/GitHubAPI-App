7ebd6439760efcccd0e4b222cbde89c074e287a1
canneIIoni <paesberaldiluca@gmail.com>
2023-08-16 15:02:46 -0300

    Connected the network call to the View, with the profile image, name and bio

--- GitHub_API_App/ContentView.swift
+++ GitHub_API_App/ContentView.swift
@@ -8,22 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var user: GitHubUser?
+    
     var body: some View {
         VStack(spacing: 20){
-            Circle()
-                .foregroundColor(.secondary)
-                .frame(width: 120, height: 120)
             
-            Text("Username")
+            AsyncImage(url: URL(string: user?.avatarUrl ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            } placeholder: {
+                Circle()
+                    .foregroundColor(.secondary)
+            }
+            .frame(width: 120, height: 120)
+            
+            
+            Text(user?.login ?? "Login Placeholder")
                 .bold()
                 .font(.title3)
             
-            Text("This is where the GitHub bio will go, Let's make it long so it spans two lines.")
+            Text(user?.bio ?? "Bio Placeholder")
                 .padding()
             
             Spacer()
         }
         .padding()
+        .task {
+            do{
+                user = try await getUser()
+            }catch GHError.invalidURL{
+                print("Invalid URL")
+            }catch GHError.invalidResponse{
+                print("Invalid Response")
+            }catch GHError.invalidData{
+                print("Invalid Data")
+            }catch{
+                print("Unexpected Error")
+            }
+        }
     }
     
     func getUser() async throws -> GitHubUser {
